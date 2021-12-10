b638323f5e246f1df16a1321751be2e9e4604577
Pankajazlearn <49351041+Pankajazlearn@users.noreply.github.com>
2021-12-10 00:14:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
 locals {
   enabled = module.this.enabled
 
